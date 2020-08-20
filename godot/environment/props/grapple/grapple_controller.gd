64bc828bac05871e17d819e2b0389f4ce5adce5b
SadmanTariq <sadmantariq@gmail.com>
2020-08-20 21:32:17 +0600

    Grapple line of sight detection

--- godot/environment/props/grapple/grapple_controller.gd
+++ godot/environment/props/grapple/grapple_controller.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 const activation_range = 600
 
@@ -10,11 +10,14 @@ func _physics_process(_delta):
 	var nearest_anchor: Node2D
 	var nearest_dist = INF
 	
+	var space_state = get_world_2d().direct_space_state
+	
 	for anchor in get_tree().get_nodes_in_group("grapple_anchor"):
-		var dist = player.global_position.distance_to(anchor.global_position)
-		if dist < nearest_dist:
-			nearest_anchor = anchor
-			nearest_dist = dist
+		if !space_state.intersect_ray(player.global_position, anchor.global_position, [player]):
+			var dist = player.global_position.distance_to(anchor.global_position)
+			if dist < nearest_dist:
+				nearest_anchor = anchor
+				nearest_dist = dist
 	
 	if nearest_dist > activation_range:
 		return
