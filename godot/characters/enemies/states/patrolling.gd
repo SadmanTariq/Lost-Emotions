ee8a0a5f6488c22b2005883c38e6932288d7c937
SadmanTariq <sadmantariq@gmail.com>
2020-08-21 11:20:22 +0600

    Enemy base class with patrolling

---
+++ godot/characters/enemies/states/patrolling.gd
@@ -0,0 +1,27 @@
+extends GameState
+
+enum Direction {
+	LEFT,
+	RIGHT
+}
+
+export(Direction) var direction = Direction.RIGHT
+export(NodePath) var right_ray_path
+export(NodePath) var left_ray_path
+export var speed = 200
+
+var acting_body: KinematicBody2D
+onready var right_ray: RayCast2D = get_node(right_ray_path)
+onready var left_ray:  RayCast2D = get_node(left_ray_path)
+
+func physics_process(_delta):
+	_check_direction()
+	var velocity = [-1, 1][direction] * Vector2.RIGHT * speed
+	velocity.y = 10
+	velocity = acting_body.move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+
+func _check_direction():
+	if direction == Direction.RIGHT and !right_ray.is_colliding():
+		direction = Direction.LEFT
+	elif direction == Direction.LEFT and !left_ray.is_colliding():
+		direction = Direction.RIGHT
