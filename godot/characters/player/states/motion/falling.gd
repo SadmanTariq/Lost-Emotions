2360452b2378250ff1fe8358a432eeb3c804fcb3
SadmanTariq <sadmantariq@gmail.com>
2020-08-23 14:44:23 +0600

    Limit fall speed

--- godot/characters/player/states/motion/falling.gd
+++ godot/characters/player/states/motion/falling.gd
@@ -1,5 +1,6 @@
 extends "res://characters/player/states/motion/aerial.gd"
 
+export var max_fall_speed = 500
 export var coyote_duration = 0.2
 
 func input(event: InputEvent):
@@ -7,6 +8,9 @@ func input(event: InputEvent):
 	if event.is_action_pressed("jump") and !$Timer.is_stopped():
 		fsm.context["jumps_left"] += 1
 
+func physics_process(delta):
+	.physics_process(delta)
+	fsm.context[velocity_key].y = min(fsm.context[velocity_key].y, max_fall_speed)
 
 func _on_Walking_walked_off_ledge():
 	$Timer.start(coyote_duration)
