1ee3b6658ebc5e50deb62016c5b28e2303ee2394
SadmanTariq <sadmantariq@gmail.com>
2020-08-23 22:13:11 +0600

    FIX THE INDICATORS

--- godot/GUI/ability_indicator_script/VBoxContainer.gd
+++ godot/GUI/ability_indicator_script/VBoxContainer.gd
@@ -1,37 +1,8 @@
 extends VBoxContainer
 
-onready var dash = get_node("Dash")
-onready var double_jump = get_node("Double Jump")
-onready var teleport = get_node("Teleport")
-
-
-#onready var is_dash_available = get_node("../../World/Player").dash_available
-#onready var is_double_jump_available = get_node("../../World/Player").double_jump_available
-#onready var is_teleport_available = get_node("../../World/Player").teleport_available
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
+func _process(_delta):
 	var player = Globals.player
 	
-	dash.visible = player.dash_available
-	double_jump.visible = player.double_jump_available
-	teleport.visible = player.teleport_available
-#	if is_dash_available:
-#		dash.visible = true
-#	else :
-#		dash.visible = false
-#
-#	if is_double_jump_available:
-#		double_jump.visible = true
-#	else:
-#		double_jump.visible = false
-#
-#
-#	if is_teleport_available:
-#		teleport.visible = true
-#	else:
-#		teleport.visible = false
+	$Dash.visible = player.dash_available
+	$DoubleJump.visible = player.double_jump_available
+	$Teleport.visible = player.teleport_available
