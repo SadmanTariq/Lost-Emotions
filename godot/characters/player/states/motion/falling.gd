ca1faf2c125190708753384b9794204a877421cc
SadmanTariq <sadmantariq@gmail.com>
2020-08-23 16:02:35 +0600

    Fix coyote

--- godot/characters/player/states/motion/falling.gd
+++ godot/characters/player/states/motion/falling.gd
@@ -2,11 +2,19 @@ extends "res://characters/player/states/motion/aerial.gd"
 
 export var max_fall_speed = 500
 export var coyote_duration = 0.2
+var coyote = false
+
+func enter():
+	.enter()
+	coyote = false
 
 func input(event: InputEvent):
 	.input(event)
 	if event.is_action_pressed("jump") and !$Timer.is_stopped():
-		fsm.context["jumps_left"] += 1
+		print("coyote")
+		coyote = true
+#		fsm.context["jumps_left"] += 1
+		fsm.change_to("Jumping")
 
 func physics_process(delta):
 	.physics_process(delta)
