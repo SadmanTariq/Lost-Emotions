d1fbf87b7abc7dd54c8d8960977b94ed614188b8
SadmanTariq <sadmantariq@gmail.com>
2020-08-23 21:49:32 +0600

    Aasdfajhknbqr,xzcvip

--- godot/GUI/ability_indicator_script/VBoxContainer.gd
+++ godot/GUI/ability_indicator_script/VBoxContainer.gd
@@ -5,9 +5,9 @@ onready var double_jump = get_node("Double Jump")
 onready var teleport = get_node("Teleport")
 
 
-onready var is_dash_available = get_node("../../World/Player").dash_available
-onready var is_double_jump_available = get_node("../../World/Player").double_jump_available
-onready var is_teleport_available = get_node("../../World/Player").teleport_available
+#onready var is_dash_available = get_node("../../World/Player").dash_available
+#onready var is_double_jump_available = get_node("../../World/Player").double_jump_available
+#onready var is_teleport_available = get_node("../../World/Player").teleport_available
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,18 +15,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
-	if is_dash_available:
-		dash.visible = true
-	else :
-		dash.visible = false
+	var player = Globals.player
 	
-	if is_double_jump_available:
-		double_jump.visible = true
-	else:
-		double_jump.visible = false
-	
-	
-	if is_teleport_available:
-		teleport.visible = true
-	else:
-		teleport.visible = false
+	dash.visible = player.dash_available
+	double_jump.visible = player.double_jump_available
+	teleport.visible = player.teleport_available
+#	if is_dash_available:
+#		dash.visible = true
+#	else :
+#		dash.visible = false
+#
+#	if is_double_jump_available:
+#		double_jump.visible = true
+#	else:
+#		double_jump.visible = false
+#
+#
+#	if is_teleport_available:
+#		teleport.visible = true
+#	else:
+#		teleport.visible = false
