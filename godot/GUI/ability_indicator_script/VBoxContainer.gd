67a497778bfda799a634242b3443d1f850600f4a
masrur-mmrs <masrur.mmrs@gmail.com>
2020-08-23 19:23:25 +0600

    added the indicator thingies

---
+++ godot/GUI/ability_indicator_script/VBoxContainer.gd
@@ -0,0 +1,32 @@
+extends VBoxContainer
+
+onready var dash = get_node("Dash")
+onready var double_jump = get_node("Double Jump")
+onready var teleport = get_node("Teleport")
+
+
+onready var is_dash_available = get_node("../../World/Player").dash_available
+onready var is_double_jump_available = get_node("../../World/Player").double_jump_available
+onready var is_teleport_available = get_node("../../World/Player").teleport_available
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	if is_dash_available:
+		dash.visible = true
+	else :
+		dash.visible = false
+	
+	if is_double_jump_available:
+		double_jump.visible = true
+	else:
+		double_jump.visible = false
+	
+	
+	if is_teleport_available:
+		teleport.visible = true
+	else:
+		teleport.visible = false
