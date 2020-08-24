4a4bc73b14e82449c803d892f84d59638818bf26
SadmanTariq <sadmantariq@gmail.com>
2020-08-24 19:01:18 +0600

    Naming scheme hell

--- godot/environment/props/dolphin/dolphin.gd
+++ godot/environment/props/dolphin/dolphin.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-export var end_screen = "res://GUI/Scenes/End Screen.tscn"
+export var end_screen = "res://gui/scenes/end_screen.tscn"
 
 func _on_Dolphin_body_entered(body):
 	if body == Globals.player:
--- godot/gui/buttons/return_to_main.gd
+++ godot/gui/buttons/return_to_main.gd
@@ -8,4 +8,4 @@ onready var scene = get_node("SceneChanger")
 func _on_Return_to_main_pressed() -> void:
 	var new_pause_state = not get_tree().paused
 	get_tree().paused = new_pause_state
-	scene.change_scene("res://gui/scenes/Title Screen.tscn")
+	scene.change_scene("res://gui/scenes/title_screen.tscn")
